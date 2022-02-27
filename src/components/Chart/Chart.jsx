d990149ee75346090c036933102f32502e5c334f
John Young <john@tikaro.net>
2022-02-27 11:56:40 -0500

    Add tooltips

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTooltip } from 'victory';
 
 import Precincts from '../../json/precincts.json';
 
 export const Chart = (props) => {
+
+    function tooltipMessage(datum) {
+      let precinctMessage = `Precinct ${datum.precinct_number}`;
+      if (datum.precintName !== "") { precinctMessage += `\n${datum.precinct_name}`};
+      precinctMessage += `\n${datum.registered.toLocaleString("en-US")} voters`;
+      return precinctMessage;
+    }
 
     return (
     <VictoryChart
@@ -15,10 +22,12 @@ export const Chart = (props) => {
           dependentAxis
           tickFormat={(y) => (y)}
         />
-        <VictoryBar 
-            data={Precincts}
-            x="precinct_number"
-            y="registered"
+        <VictoryBar
+          data={Precincts}
+          x="precinct_number"
+          y="registered"
+          labels={({ datum }) => tooltipMessage(datum)}
+          labelComponent={<VictoryTooltip/>}
         />
     </VictoryChart>
     );
